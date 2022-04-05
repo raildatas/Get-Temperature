4af96c10971845a0672e802662f67376604c560e
Chengqiruif <102952060+Chengqiruif@users.noreply.github.com>
2022-04-05 12:42:06 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace wdj
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbltmp = new System.Windows.Forms.Label();
+            this.lblnum = new System.Windows.Forms.Label();
+            this.lblcity = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lbltmp
+            // 
+            this.lbltmp.AutoSize = true;
+            this.lbltmp.Font = new System.Drawing.Font("微软雅黑", 26.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lbltmp.Location = new System.Drawing.Point(597, 51);
+            this.lbltmp.Name = "lbltmp";
+            this.lbltmp.Size = new System.Drawing.Size(135, 113);
+            this.lbltmp.TabIndex = 0;
+            this.lbltmp.Text = "℃";
+            // 
+            // lblnum
+            // 
+            this.lblnum.AutoSize = true;
+            this.lblnum.Font = new System.Drawing.Font("微软雅黑", 26.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblnum.Location = new System.Drawing.Point(442, 51);
+            this.lblnum.Name = "lblnum";
+            this.lblnum.Size = new System.Drawing.Size(162, 113);
+            this.lblnum.TabIndex = 1;
+            this.lblnum.Text = "---";
+            // 
+            // lblcity
+            // 
+            this.lblcity.AutoSize = true;
+            this.lblcity.Font = new System.Drawing.Font("微软雅黑", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblcity.Location = new System.Drawing.Point(12, 9);
+            this.lblcity.Name = "lblcity";
+            this.lblcity.Size = new System.Drawing.Size(357, 181);
+            this.lblcity.TabIndex = 2;
+            this.lblcity.Text = "城市";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(762, 197);
+            this.Controls.Add(this.lblcity);
+            this.Controls.Add(this.lblnum);
+            this.Controls.Add(this.lbltmp);
+            this.Name = "Form1";
+            this.Text = "温度查询系统";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbltmp;
+        private System.Windows.Forms.Label lblnum;
+        private System.Windows.Forms.Label lblcity;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wdj
+{
+    public partial class Form1 : Form
+    {
+        string english = "";
+        WebClient wc = new WebClient();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ~Form1()
+        {
+            wc.Dispose();
+            GC.Collect();
+        }
+
+        private string GetIP()
+        {
+            string pageHtml = string.Empty;
+            try
+            {
+                using (WebClient MyWebClient = new WebClient())
+                {
+                    Encoding encode = Encoding.GetEncoding("gbk");
+                    MyWebClient.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.84 Safari/537.36");
+                    MyWebClient.Credentials = CredentialCache.DefaultCredentials;//获取或设置用于向Internet资源的请求进行身份验证的网络凭据
+                    Byte[] pageData = MyWebClient.DownloadData("http://www.net.cn/static/customercare/yourip.asp"); //从指定网站下载数据
+                    pageHtml = encode.GetString(pageData);
+                }
+            }
+            catch { }
+            string reg = @"(?:(?:(25[0-5])|(2[0-4]\d)|((1\d{2})|([1-9]?\d)))\.){3}(?:(25[0-5])|(2[0-4]\d)|((1\d{2})|([1-9]?\d)))";
+            string ip = "";
+            Match m = Regex.Match(pageHtml, reg);
+            if (m.Success)
+            {
+                ip = m.Value;
+            }
+            return ip;
+        }
+        private Color GetColor(double tmp)
+        {
+            Color color = new Color();
+            if(tmp < 15&&tmp > -51)
+            {
+                color = Color.FromArgb(0, 0, (int)((65 - (tmp + 50)) * 3));
+            }
+            else if(tmp < 26&&tmp >14)
+            {
+                color = Color.FromArgb(0, (int)((11 - (tmp - 14)) * 11)+127, 0);
+            }
+            else
+            {
+                color = Color.FromArgb((int)(255-((25 - (tmp - 14)) * 5)), 0, 0);
+            }
+            GC.Collect();
+            return color;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string html = wc.DownloadString("https://www.tianqi.com/" + english + "/");
+            string[] lines = html.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            // <p class="now"><b>25</b><i>℃</i></p>
+            string regex = "<p class=\"now\"><b>\\d</b><i>℃</i></p>";
+            Regex r = new Regex("\\t\\t\\t\\t<p class=\"now\"><b>\\d</b><i>℃</i></p>");
+            int index = 152;
+            int s = regex.IndexOf("\\d");
+            int l = regex.LastIndexOf("</b>") - regex.IndexOf("\\d");
+            string strC = lines[index].Substring(s, l);
+            double c = double.Parse(strC);
+            lbltmp.ForeColor = GetColor(c);
+            lblnum.ForeColor = GetColor(c);
+            lblnum.Text = c.ToString();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string http = "https://www.ip138.com/iplookup.asp?ip=" + GetIP() + "&action=2";
+            byte[] buffer = wc.DownloadData(http);
+            string show = Encoding.Default.GetString(buffer);
+            string[] temp = show.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            string city = temp[25].Substring(33, temp[25].IndexOf("市") - 33);
+            lblcity.Text = city;
+            english = PinYin.Trim(PinYin.ToLower(PinYin.RemoveAttributeChars(PinYin.ToPinYin(city))));
+            string html = wc.DownloadString("https://www.tianqi.com/" + english + "/");
+            string[] lines = html.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            // <p class="now"><b>25</b><i>℃</i></p>
+            string regex = "<p class=\"now\"><b>\\d</b><i>℃</i></p>";
+            Regex r = new Regex("\\t\\t\\t\\t<p class=\"now\"><b>\\d</b><i>℃</i></p>");
+            int index = 152;
+            int s = regex.IndexOf("\\d");
+            int l = regex.LastIndexOf("</b>") - regex.IndexOf("\\d");
+            string strC = lines[index].Substring(s, l);
+            double c = double.Parse(strC);
+            lbltmp.ForeColor = GetColor(c);
+            lblnum.ForeColor = GetColor(c);
+            lblnum.Text = c.ToString();
+        }
+    }
+}
